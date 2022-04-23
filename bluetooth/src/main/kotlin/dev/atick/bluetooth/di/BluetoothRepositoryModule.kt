a759dfdfa901aac853d6a58200f59afc31a9b004
Atick Faisal <atickfaisal@gmail.com>
2022-04-24 01:37:31 +0300

    Fix bluetooth disconnecting issue

--- bluetooth/src/main/kotlin/dev/atick/bluetooth/di/BluetoothRepositoryModule.kt
+++ bluetooth/src/main/kotlin/dev/atick/bluetooth/di/BluetoothRepositoryModule.kt
@@ -6,11 +6,13 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dev.atick.bluetooth.repository.BtManager
 import dev.atick.bluetooth.repository.BtManagerImpl
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class BluetoothRepositoryModule {
     @Binds
+    @Singleton
     abstract fun bindBluetoothRepository(
         bluetoothRepositoryImpl: BtManagerImpl
     ): BtManager
